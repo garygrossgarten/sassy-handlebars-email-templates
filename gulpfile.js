7d8145999bae9f34c752d8b86ff9731ca22171a8
Tony Ranieri <tony@ranieri.me>
2016-05-19 12:48:32 -0500

    first working attempt at compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
 var $ = require('gulp-load-plugins')();
 
 // compile sass
@@ -19,11 +20,23 @@ gulp.task('sass', function () {
 
 // compile templates
 gulp.task('handlebars', ['sass'], function () {
+
+    var templateData = {
+        title: 'Test Tamplate',
+        body: 'Real data would go here, but this is a test...'
+    },
+        options = {
+            ignorePartials: true,
+            batch: ['./src/templates/partials']
+        }
+
     gulp.src('src/templates/**/*.hbs')
-        .pipe($.handlebars())
+        .pipe(handlebars(templateData, options))
+        .pipe($.rename('sample.html'))
         .pipe(gulp.dest('build'))
         .pipe($.juice())
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('default', ['handlebars']);
+
