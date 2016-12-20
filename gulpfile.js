730bf533bdbdfc64aa4783330319c50150505e2e
Tony Ranieri <tony@ranieri.me>
2016-12-20 11:22:23 -0600

    updated gulp processing to pull template-data from template file with same name & .json extension

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars'); // not sure why but this doesn't seem to play nice with gulp-load-plugins
 var rimraf = require('rimraf').sync;
 var $ = require('gulp-load-plugins')();
+var data = require('gulp-data');
+var path = require('path');
 
 // compile sass
 gulp.task('sass', function () {
@@ -22,16 +24,17 @@ gulp.task('sass', function () {
 // compile templates
 gulp.task('handlebars', ['sass'], function () {
 
-    var templateData = {
-            title: 'Test Template',
-            body: 'Real data would go here, but this is a test...'
-        },
+    var templateData = {},   // setting template data is supposed to extend data found in template-data json files
         options = {
             ignorePartials: false,
             batch: ['./src/templates/partials']
         }
 
     gulp.src('src/templates/*.hbs')
+        .pipe(data(function (file) {
+            // get the json file equivalent for template data
+            return require('./src/template-data/' + path.basename(file.path, '.hbs') + '.json');
+        }))
         .pipe(handlebars(templateData, options))
         .pipe($.rename('sample.html'))
         .pipe(gulp.dest('./_build'))
